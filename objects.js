c75a6d454d55ca1dc5177b295f1b8abc5c33f249
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-09-26 09:23:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
-var playlist = { artistName: songTitle };
+var playlist = { 'artistName1': 'songTitle1', 'artistName2': 'songTitle2' };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist = artistName.songTitle;
-  return playlist;
+  playlist[artistName] = songTitle
+  return playlist
 }
